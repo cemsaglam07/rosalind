d59b2d6ef5d5b0b848c42cab4182105572b1daa0
Cem Sağlam <68742964+cemsaglam07@users.noreply.github.com>
2021-08-21 11:12:37 +0300

    Create ins.py

---
+++ algorithmic-heights/ins.py
@@ -0,0 +1,16 @@
+with open("rosalind_ins.txt", "r") as f:
+    n = int(f.readline())
+    a = f.readline().split()
+    a = [int(i) for i in a]
+
+count = 0
+for i in range(1, n):
+    k = i
+    while k > 0 and a[k] < a[k-1]:
+        # swap a[k] and a[k-1]
+        a[k], a[k-1] = a[k-1], a[k]
+        count += 1
+        k = k - 1
+
+with open("rosalind_ins_output.txt", "w") as ff:
+    print(count, file=ff)
